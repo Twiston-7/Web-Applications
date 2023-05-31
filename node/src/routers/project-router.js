cf0eb7b8180a536e5b2cb2d03c83e1f7df1cab1e
Nick <nick@ridder.club>
2023-05-31 11:53:26 +0200

    Finished week 4 by adding SoC

---
+++ node/src/routers/project-router.js
@@ -0,0 +1,10 @@
+// File: routers/cat-router.js
+import express from "express";
+// Create a new router using express
+const
+    router = express.Router();
+// Calls go here
+
+
+// A default export of the router
+export default router;
